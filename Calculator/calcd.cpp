49d4c03f5ddfe81831b44f7f3bf49560a68d8682
Mateus Campos <mateusrcampos@gmail.com>
2022-09-29 22:16:05 -0300

    pushBotton1

--- Calculator/calcd.cpp
+++ Calculator/calcd.cpp
@@ -14,11 +14,14 @@ Calcd::~Calcd()
     delete ui;
 }
 
+void Calcd::on_B0_clicked()
+{
+    ui->LCDnumber->display(0);
+}
 
 void Calcd::on_B1_clicked()
 {
     ui->LCDnumber->display(1);
-
 }
 
 void Calcd::on_B2_clicked()
@@ -26,12 +29,38 @@ void Calcd::on_B2_clicked()
     ui->LCDnumber->display(2);
 }
 
+void Calcd::on_B3_clicked()
+{
+    ui->LCDnumber->display(3);
+}
 
+void Calcd::on_B4_clicked()
+{
+    ui->LCDnumber->display(4);
+}
+
+void Calcd::on_B5_clicked()
+{
+    ui->LCDnumber->display(5);
+}
+
+void Calcd::on_B6_clicked()
+{
+    ui->LCDnumber->display(6);
+}
 
 void Calcd::on_B7_clicked()
 {
     ui->LCDnumber->display(7);
 }
 
+void Calcd::on_B8_clicked()
+{
+    ui->LCDnumber->display(8);
+}
 
+void Calcd::on_B9_clicked()
+{
+    ui->LCDnumber->display(9);
+}
 
--- Calculator/calcd.h
+++ Calculator/calcd.h
@@ -16,11 +16,25 @@ public:
     ~Calcd();
 
 private slots:
+    void on_B0_clicked();
+
     void on_B1_clicked();
+
+    void on_B2_clicked();
+
+    void on_B3_clicked();
+
+    void on_B4_clicked();
+
+    void on_B5_clicked();
+
+    void on_B6_clicked();
 
     void on_B7_clicked();
 
-    void on_B2_clicked();
+    void on_B8_clicked();
+
+    void on_B9_clicked();
 
 private:
     Ui::Calcd *ui;
