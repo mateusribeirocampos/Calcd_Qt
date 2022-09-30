188b3c318e6f8fbb6f4e9b38d3a27b24b7ea4fcf
Mateus Campos <mateusrcampos@gmail.com>
2022-09-30 17:41:51 -0300

    update calcd.cpp

--- Calculator/calcd.cpp
+++ Calculator/calcd.cpp
@@ -1,5 +1,4 @@
 #include "calcd.h"
-#include "./ui_calcd.h"
 #include "ui_calcd.h"
 
 Calcd::Calcd(QWidget *parent)
@@ -14,82 +13,82 @@ Calcd::~Calcd()
     delete ui;
 }
 
-void Calcd::on_B0_clicked()
+void Calcd::B0_clicked()
 {
     ui->LCDnumber->display(0);
 }
 
-void Calcd::on_B1_clicked()
+void Calcd::B1_clicked()
 {
     ui->LCDnumber->display(1);
 }
 
-void Calcd::on_B2_clicked()
+void Calcd::B2_clicked()
 {
     ui->LCDnumber->display(2);
 }
 
-void Calcd::on_B3_clicked()
+void Calcd::B3_clicked()
 {
     ui->LCDnumber->display(3);
 }
 
-void Calcd::on_B4_clicked()
+void Calcd::B4_clicked()
 {
     ui->LCDnumber->display(4);
 }
 
-void Calcd::on_B5_clicked()
+void Calcd::B5_clicked()
 {
     ui->LCDnumber->display(5);
 }
 
-void Calcd::on_B6_clicked()
+void Calcd::B6_clicked()
 {
     ui->LCDnumber->display(6);
 }
 
-void Calcd::on_B7_clicked()
+void Calcd::B7_clicked()
 {
     ui->LCDnumber->display(7);
 }
 
-void Calcd::on_B8_clicked()
+void Calcd::B8_clicked()
 {
     ui->LCDnumber->display(8);
 }
 
-void Calcd::on_B9_clicked()
+void Calcd::B9_clicked()
 {
     ui->LCDnumber->display(9);
 }
 
-void Calcd::on_Bdot_clicked()
+void Calcd::Bdot_clicked()
 {
     ui->LCDnumber->display(".");
 }
 
-void Calcd::on_Bcomma_clicked()
+void Calcd::Bcomma_clicked()
 {
     ui->LCDnumber->display(",");
 }
 
-void Calcd::on_Bplus_clicked()
+void Calcd::Bplus_clicked()
 {
     ui->LCDnumber->display("+");
 }
 
-void Calcd::on_Bminus_clicked()
+void Calcd::Bminus_clicked()
 {
     ui->LCDnumber->display("-");
 }
 
-void Calcd::on_Bmultiple_clicked()
+void Calcd::Bmultiple_clicked()
 {
     ui->LCDnumber->display("*");
 }
 
-void Calcd::on_Bdivide_clicked()
+void Calcd::Bdivide_clicked()
 {
     ui->LCDnumber->display("/");
 }
