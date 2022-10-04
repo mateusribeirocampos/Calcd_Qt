8fe3c6420e6642c3bd86d3270acc296671f27a41
Mateus Campos <mateusrcampos@gmail.com>
2022-10-03 21:40:15 -0300

    New layout update

--- Calculator/calcd.h
+++ Calculator/calcd.h
@@ -16,39 +16,6 @@ public:
     ~Calcd();
 
 
-private slots:
-    void B0_clicked();
-
-    void B1_clicked();
-
-    void B2_clicked();
-
-    void B3_clicked();
-
-    void B4_clicked();
-
-    void B5_clicked();
-
-    void B6_clicked();
-
-    void B7_clicked();
-
-    void B8_clicked();
-
-    void B9_clicked();
-
-    void Bdot_clicked();
-
-    void Bcomma_clicked();
-
-    void Bplus_clicked();
-
-    void Bminus_clicked();
-
-    void Bmultiple_clicked();
-
-    void Bdivide_clicked();
-
 private:
     Ui::Calcd *ui;
 };
